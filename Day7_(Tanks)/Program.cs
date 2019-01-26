965e1620d18fd07cc0817987098d0680dbcbbcfd
ersyn_sultanbaev <ersyn_sultanbaev@mail.ru>
2019-01-27 00:29:42 +0600

    Добавьте файлы проекта.

---
+++ Day7_(Tanks)/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib.WordOfTanks;
+
+namespace Day7__Tanks_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tankName;
+            int tankLevelAmunition;
+            int tankLevelArmor;
+            int tankLevelAgility;
+
+            Random random = new Random();
+            int numberOfTanks = 5;
+            string winner;
+
+            int allyWinCounter = 0;
+            int enemyWinCounter = 0;
+
+            bool continueGame = true;
+            int gameContinue;
+
+            while (continueGame)
+            {
+                for (int i = 1; i <= numberOfTanks; i++)
+                {
+                    tankName = "T-34";
+                    tankLevelAmunition = random.Next(0, 100);
+                    tankLevelArmor = random.Next(0, 100);
+                    tankLevelAgility = random.Next(0, 100);
+                    Tank tankAlly = new Tank(tankName, tankLevelAmunition,
+                        tankLevelArmor, tankLevelAgility);
+                    tankAlly.ShowTankParametrs();
+
+                    tankName = "Pantera";
+                    tankLevelAmunition = random.Next(0, 100);
+                    tankLevelArmor = random.Next(0, 100);
+                    tankLevelAgility = random.Next(0, 100);
+                    Tank tankEnemy = new Tank(tankName, tankLevelAmunition,
+                        tankLevelArmor, tankLevelAgility);
+                    tankEnemy.ShowTankParametrs();
+
+                    winner = tankAlly * tankEnemy;
+                    Console.WriteLine("Битву выиграл: " + winner);
+
+                    Console.WriteLine();
+                    Console.WriteLine("###################################");
+                    if (winner == tankAlly.TankName)
+                    {
+                        ++allyWinCounter;
+                    }
+
+                    if (winner == tankEnemy.TankName)
+                    {
+                        ++enemyWinCounter;
+                    }
+                }
+
+                if (allyWinCounter > enemyWinCounter)
+                {
+                    Console.WriteLine("Победил: T-34 !!!");
+                }
+                else if (allyWinCounter < enemyWinCounter)
+                {
+                    Console.WriteLine("Победил: Pantera !!!");
+                }
+
+                else
+                {
+                    Console.WriteLine("Ни чья !!!");
+                }
+
+                Console.WriteLine("Хотите еще с играть?");
+                Console.WriteLine("ДА - 1");
+                Console.WriteLine("НЕТ - 0");
+                gameContinue = int.Parse(Console.ReadLine());
+
+                if (gameContinue>0)
+                {
+                    System.Console.Clear();
+
+                }
+                else
+                {
+                    continueGame = false;
+                }
+            }
+        }
+    }
+}
---
+++ MyClassLib/WordOfTanks/Tank.cs
@@ -0,0 +1,101 @@
+﻿namespace MyClassLib.WordOfTanks
+{
+    public class Tank
+    {
+        public Tank(string tankName, int tankLevelAmunition,
+            int tankLevelArmor, int tankLevelAgility)
+        {
+            TankName = tankName;
+            TankLevelAmunition = tankLevelAmunition;
+            TankLevelArmor = tankLevelArmor;
+            TankLevelAgility = tankLevelAgility;
+
+        }
+        public string TankName { get; set; }
+        public int TankLevelAmunition { get; set; }
+        public int TankLevelArmor { get; set; }
+        public int TankLevelAgility { get; set; }
+
+        public static string operator *(Tank tankAlly, Tank tankEnemy)
+        {
+            if (
+                (tankAlly.TankLevelAmunition > tankEnemy.TankLevelAmunition &&
+                tankAlly.TankLevelArmor > tankEnemy.TankLevelArmor &&
+                tankAlly.TankLevelAgility > tankEnemy.TankLevelAgility) ||
+
+                (tankAlly.TankLevelAmunition > tankEnemy.TankLevelAmunition &&
+                tankAlly.TankLevelArmor > tankEnemy.TankLevelArmor &&
+                tankAlly.TankLevelAgility < tankEnemy.TankLevelAgility) ||
+
+                (tankAlly.TankLevelAmunition > tankEnemy.TankLevelAmunition &&
+                tankAlly.TankLevelArmor < tankEnemy.TankLevelArmor &&
+                tankAlly.TankLevelAgility > tankEnemy.TankLevelAgility) ||
+
+                (tankAlly.TankLevelAmunition < tankEnemy.TankLevelAmunition &&
+                tankAlly.TankLevelArmor > tankEnemy.TankLevelArmor &&
+                tankAlly.TankLevelAgility > tankEnemy.TankLevelAgility)
+                )
+            {
+                return tankAlly.TankName;
+            }
+
+            else if (
+               (tankAlly.TankLevelAmunition == tankEnemy.TankLevelAmunition &&
+               tankAlly.TankLevelArmor == tankEnemy.TankLevelArmor &&
+               tankAlly.TankLevelAgility == tankEnemy.TankLevelAgility) ||
+
+               (tankAlly.TankLevelAmunition == tankEnemy.TankLevelAmunition &&
+               (
+               (tankAlly.TankLevelArmor > tankEnemy.TankLevelArmor) ||
+               (tankAlly.TankLevelArmor < tankEnemy.TankLevelArmor)
+               ) &&
+               tankAlly.TankLevelAgility == tankEnemy.TankLevelAgility) ||
+
+              (
+               ((tankAlly.TankLevelAmunition < tankEnemy.TankLevelAmunition) ||
+               (tankAlly.TankLevelAmunition > tankEnemy.TankLevelAmunition)
+              ) &&
+               tankAlly.TankLevelArmor == tankEnemy.TankLevelArmor &&
+               tankAlly.TankLevelAgility == tankEnemy.TankLevelAgility) ||
+
+               (tankAlly.TankLevelAmunition == tankEnemy.TankLevelAmunition &&
+               tankAlly.TankLevelArmor == tankEnemy.TankLevelArmor &&
+               (tankAlly.TankLevelAgility > tankEnemy.TankLevelAgility) ||
+                (tankAlly.TankLevelAgility < tankEnemy.TankLevelAgility))
+               )
+            {
+                int resultAlly = tankAlly.TankLevelArmor * tankAlly.TankLevelAmunition * tankAlly.TankLevelAgility;
+                int resultEnemy = tankEnemy.TankLevelAgility * tankEnemy.TankLevelAmunition * tankEnemy.TankLevelArmor;
+
+                if (resultAlly > resultEnemy)
+                {
+                    return tankAlly.TankName;
+                }
+                else if (resultAlly < resultEnemy)
+                {
+                    return tankEnemy.TankName;
+                }
+                else
+                {
+                    return "ни чья!";
+                }
+            }
+            else
+            {
+                return tankEnemy.TankName;
+            }
+        }
+
+        public void ShowTankParametrs()
+        {
+            System.Console.WriteLine("Танк: "+TankName);
+            System.Console.WriteLine("Боекомплект: "+ TankLevelAmunition);
+            System.Console.WriteLine("Уровень брони: "+TankLevelArmor);
+            System.Console.WriteLine("Уровень маневренности: " + TankLevelAgility);
+            System.Console.WriteLine();
+            System.Console.WriteLine("****************************");
+        }
+
+
+    }
+}
